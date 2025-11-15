9ebab459d2c13d07c83912b23b7d2f0c22fdb9f9
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2025-11-15 11:49:11 +0500

    delete todo

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 app.get('/todos', (req, res) => {
   res.json(todos);
 });
-
+//get todos with id
 app.get('/todos/:id', (req, res) => {
   // bizga sorovda kelgan idni olamz va bir yola Numberga otkazib olamz chnkki bizga u string korinishida keladi
   let todoId = Number(req.params.id);
@@ -59,6 +59,34 @@ app.post('/todos', (req, res) => {
   res.status(201).json({
     message: 'Succes todo',
     status: 201,
+  });
+});
+
+// delete todo with id
+
+app.delete('/todos/:id', (req, res) => {
+  const todoId = Number(req.params.id);
+
+  // Id bo'yicha indexni topamiz
+  const index = todos.findIndex((todo) => todo.id === todoId);
+
+  if (index === -1) {
+    return res.status(404).json({
+      message: 'Todo topilmadi',
+      status: 404,
+    });
+  }
+
+  //   ochirilayotgan todoni oldindan saqlab olamz qaydi todo ochirganini korsatish uchun
+  const deleteTodo = todos[index];
+
+  // Massivdan 1 ta elementni index bo'yicha o'chiramiz
+  todos.splice(index, 1);
+
+  // Javob qaytaramiz
+  res.json({
+    message: "Todo muvaffaqiyatli o'chirildi",
+    deleteTodo,
   });
 });
 
